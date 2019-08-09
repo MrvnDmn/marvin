5ceff52faa3850565150f0f9490616a904d6ae53
MrvnDmn <52141545+MrvnDmn@users.noreply.github.com>
2019-08-09 23:48:38 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,60 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="index.css">
+<link rel="icon" href="logo.png" type="image/png">
+<title>Home</title>
+</head>
+<header>
+<img class="profile" src="Profile.jpg" />
+<img src="header.png" class="apc" alt="APC"/>
+</header>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<body>
+<p>
+Hi, my name is Marvin Daniel S. Dimaano, you can call me Marvs or Marvin. I live in Imus, Cavite. Since today is the first day of class and it's really early in the morning, I am feeling a little sleepy. 
+During the summer this is time where I'm just preparing to go to sleep so my body haven't adjusted to the new schedule yet.
+<br>
+For the summer, I feel like I wasted most of my time, I just spent time playing games, browse the internet, watch movies. 
+I didn't really used the time to learn any new skill or at least improve the things I learned during my first year of college.
+<br>
+I really don't like talking about my feelings, I like to keep things to myself.
+I don't really like sharing with anyone unless it's someone I'm comfortable with.
+<br>
+My expectation for this subject is how can it be used or relate to the previous programming languages we have learned in the previous year.
+</p>
+</body>
+</html>
